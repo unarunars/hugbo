1a4e0922d17b09deae1357e9f2e6896392b1546c
sigursteinn0 <32844951+sigursteinn0@users.noreply.github.com>
2019-10-19 16:00:18 +0000

    Update DatabaseFetch.js

--- src/api/DatabaseFetch.js
+++ src/api/DatabaseFetch.js
@@ -1,9 +1,10 @@
 const fetch = require('isomorphic-fetch');
+export { readJSON };
 
 async function readJSON(){
     const response = await fetch('https://api.myjson.com/bins/11wwo4');
     const myJson = await response.json();
-    console.log(myJson);
     return myJson;
 }
-readJSON();
+
+async function 
